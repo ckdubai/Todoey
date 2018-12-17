166e53c1babbd5fb875b8709ddfb20eb627bd326
apple <apple@mac-pro.local>
2018-12-17 12:26:50 +0400

    add CRUD to Category

---
+++ Todoey/Controllers/CategoryTableViewController.swift
@@ -0,0 +1,94 @@
+//
+//  CategoryTableViewController.swift
+//  Todoey
+//
+//  Created by apple on 12/12/18.
+//  Copyright © 2018 Mohammed Anish. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryTableViewController: UITableViewController {
+    
+    var categoryArray = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loadItems()
+      
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let category = categoryArray[indexPath.row]
+        
+        cell.textLabel?.text = category.name
+        
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        performSegue(withIdentifier: "goToItems", sender: <#T##Any?#>)
+    }
+    
+   
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert =  UIAlertController(title: "Add Category", message: "", preferredStyle: .alert)
+        
+        let  action = UIAlertAction(title: "Add New Category", style: .default) { (action) in
+            
+            let newCategory = Category(context: self.context)
+            
+            newCategory.name = textField.text!
+            
+            self.categoryArray.append(newCategory)
+            self.saveItems()
+            
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "add new category here"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
+    func saveItems() {
+        
+        do {
+            try context.save()
+            
+        } catch {
+            print("Error saving context, \(error)")
+        }
+        //self.defaults.set(self.itemArray, forKey: "TodoListArray")
+        tableView.reloadData()
+    }
+    
+    func loadItems(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        
+        do {
+            categoryArray =  try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        
+        tableView.reloadData()
+        
+    }
+}
